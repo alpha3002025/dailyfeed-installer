fc6a75914c3389c58fac2f0571bfcc1f6bc5381e
alpha3002025 <alpha300uk@gmail.com>
2025-10-23 17:30:09 +0900

    env: 이미지 버전명을 지정해서 설치 가능하도록 수정

--- local-install-infra-and-app.sh
+++ local-install-infra-and-app.sh
@@ -1,3 +1,5 @@
+VERSION_ARG="$1"
+
 echo "🛺🛺🛺 install infrastructure ..."
 cd dailyfeed-infrastructure
 source install-local.sh
@@ -5,9 +7,19 @@ cd ..
 echo ""
 
 
-echo "🛺🛺🛺 install app ..."
-cd dailyfeed-app-helm
-source install-local.sh
-cd ..
+if [ -n "$VERSION_ARG" ]; then
+  echo "🛺🛺🛺 Installing with version: $VERSION_ARG"
+else
+  echo "🛺🛺🛺 Installing without version argument"
+fi
 echo ""
 
+echo "🛺🛺🛺 install app ..."
+cd dailyfeed-app-helm
+if [ -n "$VERSION_ARG" ]; then
+  source install-local.sh "$VERSION_ARG"
+else
+  source install-local.sh
+fi
+cd ..
+echo ""
